333c07e3078fd52e92df514d91bd17ade02c0fa2
James <jamesdev0353@outlook.com>
2021-02-11 15:05:33 +0200

    added display of products and categories

--- client/src/components/Product.vue
+++ client/src/components/Product.vue
@@ -1,31 +1,37 @@
 <template>
   <div>
-    <b-row>
+    <b-row v-for="product of products" :key="product.id">
       <column class="col-lg-3 col-md-4 col-sm-6 col-6 product">
       <router-link class="text-dark" to="/product-details">         
-        <img src="@/assets/hp.png" class="image img-prod" />
-        <p>hp elitebook 840 g1</p>
+        <img :src="product.img" class="image img-prod" />
+        <p><b>{{product.name}}</b></p>
+        <p>{{product.price}} грн</p>
       </router-link>
-      </column>
-      <column class="col-lg-3 col-md-4 col-sm-6 col-6 product">
-        <img src="@/assets/hp.png" class="image img-prod" />
-        <p>hp elitebook 840 g1</p>
-      </column>
-      <column class="col-lg-3 col-md-4 col-sm-6 col-6 product">
-        <img src="@/assets/hp.png" class="image img-prod" />
-        <p>hp elitebook 840 g1</p>
-      </column>
-      <column class="col-lg-3 col-md-4 col-sm-6 col-6 product">
-        <img src="@/assets/hp.png" class="image img-prod" />
-        <p>hp elitebook 840 g1</p>
-      </column>
-      <column class="col-lg-3 col-md-4 col-sm-6 col-6 product">
-        <img src="@/assets/hp.png" class="image img-prod" />
-        <p>hp elitebook 840 g1</p>
       </column>
     </b-row>
   </div>
 </template>
+
+<script>
+  import axios from "axios";
+export default {
+  data() {
+    return {
+       products: [],
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get("api/products");
+      this.products = res.data;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  },
+};
+
+</script>
 
 <style scoped>
 .product {
@@ -34,6 +40,7 @@
 
 p {
   text-align: center;
+  margin: 0;
 }
 
 </style>
--- client/src/views/Catalog.vue
+++ client/src/views/Catalog.vue
@@ -1,58 +1,21 @@
 <template>
   <div class="catalog">
     <h2 class="mt-4 text-center">Каталог</h2>
-
     <b-container class="mt-4">
       <b-row class="ml-0 categories-b-row">
-        <b-col class="my-3 col-lg-3 col-md-4 col-sm-6 col-6">
+        <b-col class="my-3 col-lg-3 col-md-4 col-sm-6 col-12"  v-for="category of categories" :key="category.id" >
           <router-link to="/products">
             <div class="category">
               <img
-                src="@/assets/laptop.jpg"
+                :src="category.img"
                 class="image category-img"
                 alt="category"
               />
               <div class="category-name">
-                <h5>Електроніка</h5>
+                <h5>{{category.name}}</h5>
               </div>
             </div>
           </router-link>
-        </b-col>
-        <b-col class="my-3 col-lg-3 col-md-4 col-sm-6 col-6">
-          <div class="category">
-            <img
-              src="@/assets/laptop.jpg"
-              class="image category-img"
-              alt="category"
-            />
-            <div class="category-name">
-              <h5>Електроніка</h5>
-            </div>
-          </div>
-        </b-col>
-        <b-col class="my-3 col-lg-3 col-md-4 col-sm-6 col-6">
-          <div class="category">
-            <img
-              src="@/assets/laptop.jpg"
-              class="image category-img"
-              alt="category"
-            />
-            <div class="category-name">
-              <h5>Електроніка</h5>
-            </div>
-          </div>
-        </b-col>
-        <b-col class="my-3 col-lg-3 col-md-4 col-sm-6 col-6">
-          <div class="category">
-            <img
-              src="@/assets/laptop.jpg"
-              class="image category-img"
-              alt="category"
-            />
-            <div class="category-name">
-              <h5>Електроніка</h5>
-            </div>
-          </div>
         </b-col>
       </b-row>
       <b-row> </b-row>
@@ -60,13 +23,33 @@
   </div>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get("api/categories");
+      this.categories = res.data;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  },
+};
+</script>
 <style>
-
 .catalog {
   min-height: 100vh;
 }
 .category-img {
-  width: 100%;
+  width: 40vh;
+  height: 25vh;
+  object-fit: cover;
   position: relative;
   border-radius: 50px;
   filter: brightness(20%);
@@ -89,5 +72,19 @@
   position: relative;
   text-align: center;
   padding: 0px;
+}
+
+@media (max-width: 768px) {
+  .category-img {
+      width: 70vh;
+      height: 40vh;
+  }
+
+  .category-name {
+  width: 50%;
+  font-size: 10px;
+  text-align: left;
+  margin: 10%;
+}
 }
 </style>
