3c9e1faeb88cc3b6e03f0a0cd14a9980ea21fbd5
James <jamesdev0353@outlook.com>
2021-02-12 10:39:53 +0200

    fixed bug

--- client/src/views/Products.vue
+++ client/src/views/Products.vue
@@ -30,7 +30,7 @@
         </b-column>
 
         <b-column class="col-md-9 col-sm-12 products">
-          <Product v-bind:id="$route.params.subcategory_id" />
+          <Product v-bind:subcategory_id="$route.params.subcategory_id" />
         </b-column>
       </b-row>
     </b-container>
