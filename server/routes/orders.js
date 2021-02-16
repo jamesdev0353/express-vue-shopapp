d458745541b046d38aeb867c5e5766ac50e50219
James <jamesdev0353@outlook.com>
2021-02-16 11:41:05 +0200

    Added user cart

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -3,10 +3,25 @@ const router = Router();
 const db = require("../config/db");
 
 router.get("/", (req, res) => {
-  // db.query(`SELECT * FROM orders`, (err, result) => {
-  // res.json(result);
-  // });
-  res.send(req.headers.token);
+  // db.query(
+  //   `SELECT * FROM products WHERE id IN
+  //     (SELECT product_id FROM orders WHERE user_id =
+  //     (SELECT id FROM users WHERE token = "${req.params.token}"))`,
+  //   (err, result) => {
+  //     res.json(result);
+  //     console.log(err);
+  //   }
+  // );
+
+  db.query(
+    `SELECT products.*, orders.count FROM products
+    INNER JOIN orders ON products.id = orders.product_id
+    INNER JOIN users ON users.id = orders.user_id WHERE users.token = "${req.headers.token}"`,
+    (err, result) => {
+      res.json(result);
+      console.log(err);
+    }
+  );
 });
 
 router.post("/", (req, res) => {
@@ -23,6 +38,8 @@ router.post("/", (req, res) => {
   //       );
   //     }
   //   );
+
+  res.send("Orders post");
 });
 
 module.exports = router;
