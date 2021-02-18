2ee37113a696432b60208e100a1ebdaded90d718
James <jamesdev0353@outlook.com>
2021-02-18 19:03:01 +0200

    information for main page

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const router = Router();
 const db = require("../config/db");
 
-router.get("/", async (req, res) => {
-  let [result] = await db.query(`SELECT * FROM products`);
-
-  res.json(result);
-});
+router.get("/", async(req, res) => {
+  let [categories] = await db.query(`SELECT id,name from categories where level = 0`)
+  var arr = [];
+  for(const element of categories){
+    let [products] = await db.query(`SELECT * FROM products where subcategory_id in (SELECT id from categories where parent_id = ${element.id}) ORDER by RAND() LIMIT 4`)
+    arr.push({name: element.name, products: products })
+  }
+  res.json(arr)
+})
 
 router.get("/:product_id", async (req, res) => {
   if (req.baseUrl == "/api/admin/delete") {
