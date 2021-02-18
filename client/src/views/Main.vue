ac59fe0a1604065f86b3cfb628b23daf09b00657
James <jamesdev0353@outlook.com>
2021-02-18 20:44:30 +0200

    Revert "added products to main page"
    This reverts commit 8501e1e8d45e67d83602807d870ded988dc51c09.

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -7,48 +7,77 @@
         <router-link to="/categories" class="button">Каталог</router-link>
       </div>
     </div>
-    <div class="category-1" v-for="item of items" :key="item">
-      <h4 class="title" v-if="item.products.length != 0">
-        <b>{{ item.name }}</b>
-      </h4>
-      <div class="container" v-if="item.products.length != 0">
-        <div class="row mt-4 row-products">
-          <column
-            class="col-lg-3 col-md-4 col-sm-6 col-6 product"
-            v-for="product of item.products"
-            :key="product"
-          >
-            <router-link class="text-dark" :to="'/products/' + product.id"
-              >
-              <img :src="product.img" class="image img-prod" />
-              <p class="mt-2 products">
-                <b>{{ product.name }}</b>
-              </p>
-              <p class="mt-1 products">{{ product.price }} грн</p>
-            </router-link>
+    <div class="category-1">
+      <h4 class="title"><b>Електроніка</b></h4>
+      <b-container>
+        <b-row class="mt-4">
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
           </column>
-        </div>
-      </div>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+        </b-row>
+      </b-container>
+    </div>
+    <div class="category-1">
+      <h4 class="title"><b>Одяг</b></h4>
+      <b-container>
+        <b-row class="mt-4">
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/clothes-1.jpg" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/clothes-1.jpg" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/clothes-1.jpg" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/clothes-1.jpg" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+        </b-row>
+      </b-container>
+    </div>
+    <div class="category-2">
+      <h4 class="title"><b>Електроніка</b></h4>
+      <b-container>
+        <b-row class="mt-4">
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+          <column class="col-md-3 col-6 product my-auto">
+            <img src="@/assets/hp.png" class="image img-prod" />
+            <p>hp elitebook 840 g1</p>
+          </column>
+        </b-row>
+      </b-container>
     </div>
   </div>
 </template>
-
-<script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      all_items: null,
-      items: null,
-    };
-  },
-  mounted() {
-    axios.get("/api/products").then((response) => {
-      this.items = response.data;
-    });
-  },
-};
-</script>
 
 <style>
 body {
@@ -71,25 +100,11 @@ body {
   width: 80%;
 }
 
-.products {
-  font-size: medium;
-  margin: 0;
-}
-
-.row-products {
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-}
-
 .product {
   height: 100%;
+  widows: 100%;
   margin-top: 20px;
   transition: transform 0.2s;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  min-height: 280px;
 }
 
 .product:hover {
@@ -138,7 +153,7 @@ b-row {
 .category-2 {
   margin: auto;
   margin-top: 20px;
-  width: 70%;
+  width: 100%;
   position: relative;
 }
 
