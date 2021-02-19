4b11d0c0d1346a842b69fe7dced74c348a533404
James <jamesdev0353@outlook.com>
2021-02-20 00:11:06 +0200

    Fix deleted categories

--- server/routes/ordersChart.js
+++ server/routes/ordersChart.js
@@ -4,9 +4,9 @@ const db = require("../config/db");
 
 router.get("/", async (req, res) => {
   let [result] = await db.query(
- `SELECT SUM(orders.count) as orderCount, date, SUM(products.price*orders.count) as income from orders inner join products ON products.id = orders.product_id where status>0 GROUP BY date`
+    `SELECT SUM(orders.count) AS orderCount, date, SUM(products.price * orders.count) AS income FROM orders INNER JOIN products ON products.id = orders.product_id WHERE status > 0 AND deleted = 0 GROUP BY date`
   );
-  res.json(result)
+  res.json(result);
 });
 
 module.exports = router;
