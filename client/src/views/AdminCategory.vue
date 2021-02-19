312af7bd21b5c4a18515889c9a4e6033b9775788
James <jamesdev0353@outlook.com>
2021-02-19 02:58:54 +0200

    fix text on buttons

--- client/src/views/AdminCategory.vue
+++ client/src/views/AdminCategory.vue
@@ -31,7 +31,7 @@
           class="sold-button ml-2"
           @click="$router.push({ name: 'Adminsolditems' })"
         >
-          Продані товари
+          Замовлені товари
         </button>
       </div>
       <div class="row" v-for="i in category" v-bind:key="i">
@@ -78,25 +78,27 @@
             </svg>
             Додати підкатегорію
           </router-link>
-          <button
-            v-if="i.level === 0"
-            @click="deleteCategory(i.id)"
-            class="add-button mt-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              class="bi bi-x-circle-fill"
-              viewBox="0 0 16 19"
+          <div class="d-flex justify-content-center link">
+            <button
+              v-if="i.level === 0"
+              @click="deleteCategory(i.id)"
+              class="add-button mt-1"
             >
-              <path
-                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"
-              />
-            </svg>
-            Видалити категорію
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                class="bi bi-x-circle-fill"
+                viewBox="0 0 16 19"
+              >
+                <path
+                  d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"
+                />
+              </svg>
+              Видалити категорію
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/views/AdminTable.vue
+++ client/src/views/AdminTable.vue
@@ -30,7 +30,7 @@
         </router-link>
         <button
             @click="deleteCategory($route.params.cat)"
-            class="add-button mt-1"
+            class="ml-4 add-button"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -44,7 +44,7 @@
                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"
               />
             </svg>
-            Видалити категорію
+            Видалити підкатегорію
          </button>
       </div>
     </div>
